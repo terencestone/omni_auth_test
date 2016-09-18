292165c3c760c861859d79caa0aa530cb9751ef1
Terence Stone <terence.stone1@gmail.com>
2016-09-18 15:00:10 -0400

    Complete test

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'aV8oNQU8FpNOssIa7jvdfqwER', 'F4TmQmdtyFBuoluuszbfqBkhlNzxXNnK1OYcjnZk5pWFgCbgMq'
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
+
+  resources :index, only: :index
+  root to: 'index#index'
+end
